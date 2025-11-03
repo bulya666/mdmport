d157ee0e0ee296c333051294886ccdc7df8dbec1
bulya666 <bulyakimate4@gmail.com>
2025-11-03 12:10:47 +0100

    Update app.routes.ts

--- mdmport/src/app/app.routes.ts
+++ mdmport/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { CommunityComponent } from './community/community.component';
 
 
 export const routes: Routes = [
     {path:"", component:MainComponent},
     {path:"login", component:LoginComponent},
-    {path:"register", component:RegisterComponent}
+    {path:"register", component:RegisterComponent},
+    {path:"community", component:CommunityComponent}
 ];
