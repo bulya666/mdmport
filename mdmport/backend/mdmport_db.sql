b04ae534baf8557a7308d40c15da6f8f8310f94f
bulya666 <bulyakimate4@gmail.com>
2025-12-04 12:13:47 +0100

    Adatbázis

--- mdmport/backend/mdmport_db.sql
+++ mdmport/backend/mdmport_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Okt 29. 15:22
+-- Létrehozás ideje: 2025. Dec 04. 12:13
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -20,13 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `mdmport_db`
 --
+
 -- --------------------------------------------------------
 
-CREATE DATABASE IF NOT EXISTS `mdmport_db`
-  DEFAULT CHARACTER SET utf8mb4
-  COLLATE utf8mb4_general_ci;
-
-use mdmport_db;
 --
 -- Tábla szerkezet ehhez a táblához `gamephotos`
 --
@@ -73,7 +69,23 @@ INSERT INTO `gamephotos` (`id`, `gameid`, `pic`) VALUES
 (47, 8, 'sotf_1.jpg'),
 (48, 8, 'sotf_2.jpg'),
 (49, 8, 'sotf_3.jpg'),
-(50, 8, 'sotf_4.jpg');
+(50, 8, 'sotf_4.jpg'),
+(51, 9, 'arc_1.jpg'),
+(52, 9, 'arc_2.jpg'),
+(53, 9, 'arc_3.jpg'),
+(54, 9, 'arc_4.jpg'),
+(55, 10, 'wwm_1.jpg'),
+(56, 10, 'wwm_2.jpg'),
+(57, 10, 'wwm_3.jpg'),
+(58, 10, 'wwm_4.jpg'),
+(59, 11, 'tw3_1.jpg'),
+(60, 11, 'tw3_2.jpg'),
+(61, 11, 'tw3_3.jpg'),
+(62, 11, 'tw3_4.jpg'),
+(64, 12, 'cyber_1.jpg'),
+(65, 12, 'cyber_2.jpg'),
+(66, 12, 'cyber_3.jpg'),
+(67, 12, 'cyber_4.jpeg');
 
 -- --------------------------------------------------------
 
@@ -102,7 +114,11 @@ INSERT INTO `games` (`id`, `title`, `tag`, `price`, `desc`, `thumbnail`) VALUES
 (5, 'Green Hell', 'all', '$20.99', 'Nyílt világú túlélőszimulátor, Amazonasi esőerdőben kell túlélned.', 'https://cdn.steamstatic.com/steam/apps/815370/capsule_616x353.jpg'),
 (6, 'Red Dead Redemption 2', 'all top', '$59.99', 'Akció-kalandjáték, amely a Vadnyugat hanyatlásának idején, 1899-ben játszódik.', 'https://cdn.steamstatic.com/steam/apps/1174180/capsule_616x353.jpg'),
 (7, 'Rust', 'all top', '$39.99', 'Online túlélőjáték, ahol egy szigeten ébredsz mindenféle felszerelés nélkül.', 'https://cdn.steamstatic.com/steam/apps/252490/capsule_616x353.jpg'),
-(8, 'Sons Of The Forest', 'all top', '$28.99', 'Nyílt világú túlélő-horror játék, ahol egy titokzatos szigeten kell élelmet gyűjtened.', 'https://cdn.steamstatic.com/steam/apps/1326470/capsule_616x353.jpg');
+(8, 'Sons Of The Forest', 'all top', '$28.99', 'Nyílt világú túlélő-horror játék, ahol egy titokzatos szigeten kell élelmet gyűjtened.', 'https://cdn.steamstatic.com/steam/apps/1326470/capsule_616x353.jpg'),
+(9, 'ARC Raiders', 'top', '$39,99', 'Többjátékos mentőakció-kaland, amelynek helyszíne egy halálos jövőbeli Föld, amelyet egy ARC néven ismert rejtélyes, gépesített fenyegetés pusztít.', 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1808500/04baafaf64a5aa5f46ecda5d71889a4848dc0628/header.jpg?t=1764755475'),
+(10, 'Where Winds Meet', 'top free', 'INGYEN', 'Wuxia-i, nyílt világú akció-kaland szerepjáték, amely a tizedik századi ókori Kínában játszódik. A játékosok egy fiatal kardmester szerepét ölthetik magukra, miközben útra kelnek, hogy feltárják saját identitásuk rejtélyeit.', 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3564740/6d94b048393d5358690a04a7db99f2c9739c703c/header.jpg?t=1763157550'),
+(11, 'The Witcher 3: Wild Hunt', 'top', '$29,99', 'Légy Ríviai Geralt, a witcher, a szörnyvadász. Vár a háború dúlta, szörnyek prédálta kontinens. Aktuális megbízásod? Megkeresi Cirit, a Prófécia Gyermekét, az élő fegyvert, aki megváltoztathatja a világ képét.', 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/292030/ad9240e088f953a84aee814034c50a6a92bf4516/header.jpg?t=1761131270'),
+(12, 'Cyberpunk 2077', 'all', '$82,78', 'Nyílt világú akciókaland-RPG, amely a hatalom, a fényűzés és a testmódosítások lázában élő jövőbeli megapoliszban, Night Cityben játszódik.', 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1091500/e9047d8ec47ae3d94bb8b464fb0fc9e9972b4ac7/header.jpg?t=1756209867');
 
 -- --------------------------------------------------------
 
@@ -121,26 +137,24 @@ CREATE TABLE `ownedg` (
 --
 
 INSERT INTO `ownedg` (`id`, `userid`, `gameid`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 2, 2),
-(4, 2, 3),
-(5, 3, 1),
-(6, 3, 3),
-(7, 3, 5),
-(8, 4, 4),
-(9, 5, 2),
-(10, 5, 5),
-(11, 6, 1),
-(12, 6, 4),
-(13, 7, 3),
-(14, 7, 4),
-(15, 7, 5),
-(16, 8, 2),
-(17, 9, 1),
-(18, 9, 2),
-(19, 9, 3),
-(20, 10, 5);
+(36, 28, 1),
+(37, 28, 2),
+(38, 28, 4),
+(39, 28, 3),
+(40, 28, 7),
+(41, 28, 5),
+(42, 28, 6),
+(43, 28, 8),
+(44, 30, 6),
+(45, 30, 1),
+(46, 30, 2),
+(47, 30, 12),
+(48, 30, 10),
+(49, 30, 9),
+(50, 30, 11),
+(51, 30, 3),
+(52, 30, 4),
+(53, 30, 8);
 
 -- --------------------------------------------------------
 
@@ -159,17 +173,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'alice', 'vN7$w9kQp2!x'),
-(2, 'bob', '8rT#z2LmY6@q'),
-(3, 'carol', 'Gq3!pV4sZx9*'),
-(4, 'dave', 'mP6^sW0bL1?r'),
-(5, 'eva', '4Jt!bK8nZp5$'),
-(6, 'frank', 'Q9#vH2xF7!l'),
-(7, 'gina', 'uR3!tY6pS8&'),
-(8, 'hank', 'B2^fM7zQ1#c'),
-(9, 'hank', 'B2^fM7zQ1#c'),
-(10, 'iris', 'pL5!nW9sH0%'),
-(11, 'jack', 'X7#qR2mT6!v');
+(28, 'Admin', '$2b$12$LxKFdUGTxSax1OQRj8o/1uqx1CwfwSeEfsOQNk.OSVJmVAlVYJtWO'),
+(29, 'Szakacs', '$2b$12$9C/g/bBswhWse8sQbqSUzeU7KZmDdGym0aUQCxMLK8Rfo8BX/MBsW'),
+(30, 'Bulya', '$2b$12$E.dJEjI.yQd3OHZTlPpZz.SAGCq99rj7lTDZ66OATMDSCCKig025W');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -210,25 +216,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `gamephotos`
 --
 ALTER TABLE `gamephotos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT a táblához `games`
 --
 ALTER TABLE `games`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT a táblához `ownedg`
 --
 ALTER TABLE `ownedg`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Megkötések a kiírt táblákhoz
